0cd0d254a10e6a7ff1119758a9cb1c8d5e447ce2
Daniel <dfdazac@gmail.com>
2019-03-29 15:07:18 +0100

    Generate 1 shape per image

--- generate.py
+++ generate.py
@@ -12,7 +12,7 @@ COLORS = [(0, 0, 210),
           (0, 150, 150)]
 
 SHAPES = ['ellipse', 'rectangle', 'polygon']
-num_shapes = len(SHAPES)
+num_shapes = 1
 
 def generate_img(img_size):
     """Generate an RGN image with shapes at random positions.
@@ -34,18 +34,18 @@ def generate_img(img_size):
     shape_idx = np.random.choice(np.arange(len(SHAPES)), size=num_shapes,
                                  replace=False)
 
-    for j, shape in enumerate(SHAPES):
-        x0, y0 = x0_all[j], y0_all[j]
-        x1 = x0 + size[j]
-        y1 = y0 + size[j]
+    for i in range(num_shapes):
+        x0, y0 = x0_all[i], y0_all[i]
+        x1 = x0 + size[i]
+        y1 = y0 + size[i]
         box = [x0, y0, x1, y1]
-        shape = SHAPES[shape_idx[j]]
+        shape = SHAPES[shape_idx[i]]
 
         if shape == 'polygon':
             box[0] = (x1 + x0) / 2
             box += [x0, y1]
 
-        getattr(drawer, shape)(box, fill=COLORS[idx_color_shape[j]])
+        getattr(drawer, shape)(box, fill=COLORS[idx_color_shape[i]])
 
     return img
 
